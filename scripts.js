6510c48a99ee2b71af8eaa1eaeaebede6ac3e797
Franz <franzj.pretorius.2003@gmail.com>
2024-05-07 11:59:08 +0200

    Fixed divider being 0 issue

--- scripts.js
+++ scripts.js
@@ -6,14 +6,15 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   try{
     const { dividend, divider } = Object.fromEntries(entries);
-    
-    if(dividend ==="" || divider === "") throw "Division not performed. Both values are required in inputs. Try again"
+
+    if(dividend ==="" || divider === "") throw  "Division not performed. Both values are required in inputs. Try again"
+    if(divider === "0") throw  "Division not performed. Invalid number provided. Try again"
 
     let resultNumber = Math.trunc(dividend / divider)
     result.innerText = resultNumber;
   }catch(err){
-    console.error(err)
     result.innerText = err
+    if(err === "Division not performed. Invalid number provided. Try again") console.trace("This line got called from")
   }
   
 
