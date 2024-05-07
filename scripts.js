c9c0c3be24c3e294ecd4137dda12cd2ae46e30b1
Franz <franzj.pretorius.2003@gmail.com>
2024-05-07 11:38:23 +0200

    Added blank input error detection

--- scripts.js
+++ scripts.js
@@ -4,9 +4,22 @@ const result = document.querySelector("[data-result]");
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
+  try{
+    const { dividend, divider } = Object.fromEntries(entries);
+    
+    if(dividend ==="" || divider === "") throw "Division not performed. Both values are required in inputs. Try again"
 
-  let resultNumber = Math.trunc(dividend / divider)
+    let resultNumber = Math.trunc(dividend / divider)
+    result.innerText = resultNumber;
+  }catch(err){
+    console.error(err)
+    result.innerText = err
+  }
+  
 
-  result.innerText = resultNumber;
+  
+
+  
+
+  
 });
